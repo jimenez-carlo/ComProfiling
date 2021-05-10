26ef2877594641c5b1d30d1815c06dd3cb2afe4f
jimenez-carlo <jimenez.carlo.llabor@gmail.com>
2021-05-10 20:15:50 +0800

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -3,9 +3,9 @@ include("connect.php");
 $email = $_GET['email'];
 if($exist = mysqli_query($con,"SELECT COUNT(*) as result FROM tbluser where email='$email' limit 1")){
   while ($res = mysqli_fetch_object($exist)) {
-  	if (!empty) {
-  		# code...
-  	}
+  	// if (!empty) {
+  	// 	# code...
+  	// }
     echo $res->result;
   }
   mysqli_free_result($exist);
